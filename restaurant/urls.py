96d6a27faaa39a91540fd227efbe34d2c6754550
Elena freire <elenafreire75@gmail.com>
2024-03-07 20:09:55 +0000

    Fix URL configuration for namespacing

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 from . import views  
 from django.contrib import admin
 
+app_name = 'restaurant'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'), 
